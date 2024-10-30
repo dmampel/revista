1ecb1c3592db4e56f4316e942019a0315413bfc9
dmampel <mampeldelfina@gmail.com>
2024-10-30 16:07:22 -0300

    retoques

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Roll} from "react-awesome-reveal";
+import { Link } from 'react-router-dom';
 const covers = [
     'davegrohl.svg', 'bowie.svg', 'red.svg', 'lana.svg', 'amy.svg', 'oasis.svg',
     'billiejoe.svg', 'marilyn.svg', 'harry.svg', 'riri.svg', 'badbunny.svg',
@@ -18,7 +19,7 @@ export default function Footer(){
                 <Roll cascade>
                     {covers.map((cover, index) => (
                     <li key={index}>
-                        <a href="http://"><img className='max-w-11 hover:-translate-y-3 transition' src={`/covers/${cover}`} alt={cover.replace('.svg', '').replace('.png', '')} /></a>
+                        <Link to='/bazaar'><img className='max-w-11 hover:-translate-y-3 transition' src={`/covers/${cover}`} alt={cover.replace('.svg', '').replace('.png', '')} /></Link>
                     </li>
                     ))}
                 </Roll>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import '../styles/index.css'
 import bg from '../assets/img/shapes.svg';
 import cover from '../assets/img/character.svg'
 import Footer from "./Footer";
+import {Slide } from "react-awesome-reveal";
 
 
 
@@ -10,12 +11,15 @@ export default function Home(){
     return(
         
         <div>
-            <div className="relative bg-contain bg-no-repeat bg-center lg:mt-24">
-                <h1 className="absolute font-serif font-extrabold text-5xl md:text-[6em] xl:text-[9em] top-72 left-5 lg:top-[6rem] lg:left-10">Rolling <strong className="text-[#CD1BB5] md:pl-[2.9rem]">Stone</strong></h1>
-                <img className='absolute max-w-[30%] md:max-w-[23%] top-36 left-[9em] md:top-10 md:left-[17em] lg:left-[19em] lg:-top-[10em] xl:left-[30em] xl:-top-[10em] 2xl:-top-10 2xl:left-[30rem]' src={cover} alt="" />
-                <img className='absolute top-56 lg:top-2 lg:left-[5em] lg:w-[70%]' src={bg} alt="" />
-                <small className="font-mono text-xs absolute font-extrabold top-[27.5em] left-6 md:top-[11rem] md:left-12 text-gray-800"> pequeño homenaje a una gran revista</small>
-            </div>
+            <Slide>
+                <div className="relative bg-contain bg-no-repeat bg-center lg:mt-24">
+                    <h1 className="absolute font-serif font-extrabold text-5xl md:text-[6em] xl:text-[9em] top-72 left-5 lg:top-[6rem] lg:left-10">Rolling <strong className="text-[#CD1BB5] md:pl-[2.9rem]">Stone</strong></h1>
+                    <img className='absolute max-w-[30%] md:max-w-[23%] top-36 left-[9em] md:top-10 md:left-[17em] lg:left-[19em] lg:-top-[10em] xl:left-[30em] xl:-top-[10em] 2xl:-top-10 2xl:left-[30rem]' src={cover} alt="" />
+                    <img className='absolute top-56 lg:top-2 lg:left-[5em] lg:w-[70%]' src={bg} alt="" />
+                    <small className="font-mono text-xs absolute font-extrabold top-[27.5em] left-6 md:top-[11rem] md:left-12 text-gray-800"> pequeño homenaje a una gran revista</small>
+                </div>
+            </Slide>
+            
             <Footer />
         </div>
     )
