79b8d6ab081767afd7c9d3052d0af317a50d3c27
dmampel <mampeldelfina@gmail.com>
2024-10-30 15:14:17 -0300

    termino bazaar :)))))))

--- src/components/Bazaar.jsx
+++ src/components/Bazaar.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { AdvancedImage } from '@cloudinary/react';
 import '../styles/bazaar.css';
-import { cld, generateImageIds, useShuffledImages } from '../hooks/covers';
+import { cld, generateImageIds} from '../hooks/covers';
 
 export default function Bazaar() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [images, setImages] = useState([]);
   
-
-  
-
   const handleMouseMove = (e) => {
     const x = (e.clientX / window.innerWidth) *40;
     const y = (e.clientY / window.innerHeight) *40;
@@ -16,15 +14,45 @@ export default function Bazaar() {
     setPosition({ x: x, y: y });
   };
 
+  useEffect(() => {
+    const ids = generateImageIds(); // Obtén los ids de las imágenes.
+    const positionedImages = ids.map(id => ({
+      id,
+      top: `${Math.random() * 260}vh`,     // Posición aleatoria en la pantalla.
+      left: `${Math.random() * 260}vw`,
+      rotation: `${Math.random() * 20 - 10}deg`, // Rotación aleatoria entre -10 y 10 grados.
+    }));
+    setImages(positionedImages);
+  }, []);
+
   return (
     <section
-        className="background"
+        className="background cursor-none"
         onMouseMove={handleMouseMove}
         style={{ transform: `translate(-${position.x}%, -${position.y}%)` }}
     >
       
-  {}
-</section>
+      {images.map(({ id, top, left, rotation }) => {
+        const img = cld.image(id).format('auto').quality('auto');
+        return (
+          
+              <AdvancedImage
+              key={id}
+              cldImg={img}
+              style={{
+                position: 'fixed',
+                top,
+                left,
+                transform: `rotate(${rotation})`,
+                transition: 'transform 0.3s ease-out',
+                width: '5%', // Ajusta el tamaño de las imágenes aquí según tus preferencias.
+              }}
+            />
+            
+          
+        );
+      })}
+    </section>
 
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar(){
 
     return(
-        <div className="px-10 py-5 bg-transparent sticky top-0">
+        <div className="px-10 py-5 bg-transparent sticky top-0 z-10">
             <ul className="flex flex-row justify-start gap-5 text-sm font-mono">
                 <li>
                     <Link to='/' className="hover:cursor-pointer hover:underline decoration-yellow-500 transition-colors ease-in-out">Home</Link>
